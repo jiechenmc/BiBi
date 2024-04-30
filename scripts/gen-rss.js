890b3a11300a4553c24c958e2090133e321ae3c5
Jie Chen <jiechenmc@gmail.com>
2024-04-30 12:21:25 -0400

    init

--- scripts/gen-rss.js
+++ scripts/gen-rss.js
@@ -5,9 +5,9 @@ const matter = require("gray-matter");
 
 async function generate() {
   const feed = new RSS({
-    title: "Your Name",
-    site_url: "https://yoursite.com",
-    feed_url: "https://yoursite.com/feed.xml",
+    title: "Jie Chen",
+    site_url: "https://blog.jiechen.dev",
+    feed_url: "https://blog.jiechen.dev/feed.xml",
   });
 
   const posts = await fs.readdir(path.join(__dirname, "..", "pages", "posts"));
