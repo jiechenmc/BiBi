212b83121b9af26b35faa07d6bfda449e6317842
Jie Chen <jiechenmc@gmail.com>
2024-04-30 13:58:48 -0400

    update

--- theme.config.js
+++ theme.config.js
@@ -1,20 +1,23 @@
 const YEAR = new Date().getFullYear();
 
 export default {
-  footer: (
-    <footer>
-      <small>
-        <time>{YEAR}</time> © Jie Chen.
-        <a href="/feed.xml">RSS</a>
-      </small>
-      <style jsx>{`
-        footer {
-          margin-top: 8rem;
-        }
-        a {
-          float: right;
-        }
-      `}</style>
-    </footer>
+  footer: <p>{YEAR} © Jie Chen.</p>,
+  head: ({ title, meta }) => (
+    <>
+      {meta.description && (
+        <meta name="description" content={meta.description} />
+      )}
+      {meta.tag && <meta name="keywords" content={meta.tag} />}
+      {meta.author && <meta name="author" content={meta.author} />}
+    </>
   ),
-};
+  readMore: 'Read More →',
+  postFooter: null,
+  darkMode: true,
+  navs: [
+    {
+      url: 'jiechen.dev',
+      name: 'About'
+    }
+  ]
+}
